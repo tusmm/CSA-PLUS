9175385b99fc776e6bee5f6e3f25f4f66befcf0b
Max Milazzo <mam9563@rit.edu>
2022-09-20 07:31:52 -0400

    added incomplete.py text copy feature

--- incomplete.py
+++ incomplete.py
@@ -11,8 +11,13 @@ for file in os.listdir("incomplete"):  # iterates through incomplete files
         os.remove(os.path.join("incomplete", file))
         # removes zip file
 
-    open(os.path.join("incomplete", file + ".txt"), "w")
-    # creates formatted comments text file in incomplete folder
+    with open("incomplete.txt", "r") as f:
+        data = f.read()
+
+    with open(os.path.join("incomplete", file + ".txt"), "w") as f:
+        f.write(data)
+
+    # creates formatted comments text file in incomplete folder (with specified text)
     # univerally creates file for ALL (non-zip included) files in folder
     # zip/py/java/etc extensions will not be removed before txt added
     # this can be added and changed if desired, but will have no final effect
